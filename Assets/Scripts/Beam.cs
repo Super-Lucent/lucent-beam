cd19fd4a612e88a0f77570deae7d84732dc8567b
William Goddard <goddard.william@gmail.com>
2014-06-25 17:47:08 +1000

    Will: Initial

---
+++ Assets/Scripts/Beam.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beam : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	private LineRenderer lr;
+
+	// Use this for initialization
+	void Start () {
+
+		lr = GetComponent<LineRenderer> ();
+		lr.SetVertexCount (2);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 p1p = player1.transform.position;
+		Vector3 p2p = player2.transform.position;
+
+
+
+		lr.SetPosition (0, p1p);
+		lr.SetPosition (1, p2p);
+
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public string xAxis;
+	public string yAxis;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Input.GetAxis (xAxis);
+		float y = Input.GetAxis (yAxis);
+
+		rigidbody2D.velocity = new Vector2 (x, y).normalized * speed;
+
+		
+	
+	}
+}
