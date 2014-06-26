dad804a9e21f547960fd7bd44fef456344abe820
Chad Toprak <chadtoprak@gmail.com>
2014-06-26 15:32:34 +1000

    Gfx + JJ WORLDSTAR

--- Assets/Scripts/Glow.cs
+++ Assets/Scripts/Glow.cs
@@ -1,42 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Beam : MonoBehaviour {
-
-	public GameObject player1;
-	public GameObject player2;
-
-	private LineRenderer lr;
-
-	private float width;
+public class Glow : MonoBehaviour {
 
 	public AnimationCurve pulseCurve;
 	public float pulseTime = 1.0f;
 	private float pulse = 0.0f;
 	private bool ping = true;
 
-
+	private float width;
 
 
 	// Use this for initialization
 	void Start () {
-
-		lr = GetComponent<LineRenderer> ();
-		lr.SetVertexCount (2);
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		Vector3 p1p = player1.transform.position;
-		Vector3 p2p = player2.transform.position;
-
-
-
-		lr.SetPosition (0, p1p);
-		lr.SetPosition (1, p2p);
-
 		if (ping) { 
 			pulse += Time.deltaTime;
 			if (pulse >  pulseTime) {
@@ -47,12 +28,13 @@ public class Beam : MonoBehaviour {
 			if (pulse < 0.0f) {
 				ping = !ping;
 			}
-
+			
 		}
 
 		width = pulseCurve.Evaluate (pulse);
 
-		lr.SetWidth (width, width);
-	
+		transform.localScale = new Vector2 (width, width);
+
+
 	}
 }
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public float rotationSpeedX,
+				 rotationSpeedY,
+				 rotationSpeedZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (rotationSpeedX * Time.deltaTime, rotationSpeedY * Time.deltaTime, rotationSpeedZ * Time.deltaTime);
+	
+	}
+}
